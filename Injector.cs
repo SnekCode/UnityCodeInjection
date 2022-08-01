d2101a3f123accfc509d8506950edcad35246ccc
SnekCode <code.snakes@gmail.com>
2022-08-01 15:33:52 -0500

    Init Commit

---
+++ Injector.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Hack
+{
+    public class Injector
+    {
+        private static GameObject _Load;
+        public static void Init()
+        {
+            _Load = new GameObject();
+            _Load.AddComponent<Main>();
+            GameObject.DontDestroyOnLoad(_Load);
+        }
+        public static void Unload()
+        {
+            _Unload();
+        }
+        private static void _Unload()
+        {
+            GameObject.Destroy(_Load);
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+namespace Hack
+{
+    class Main : MonoBehaviour
+    {
+        int count;
+        public void Start()
+        {
+
+        }
+        public void Update()
+        {
+            count++;
+        }
+        public void OnGUI()
+        {
+            // Here you can call IMGUI functions of Unity to build your UI for the hack :)
+            GUI.Label(new Rect(20f, 20f, 150f, 50f), "GAME INJECTED" + count);
+        }
+    }
+}
